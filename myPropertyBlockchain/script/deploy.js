5db91f485f1871ff05f1444320c2cb12b4a58b7a
Ashes Pokhrel <123297869+Virrous@users.noreply.github.com>
2024-11-15 19:32:47 +0545

    for abiral

--- myPropertyBlockchain/script/deploy.js
+++ myPropertyBlockchain/script/deploy.js
@@ -1,15 +1,25 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  console.log("Deploying LandRegistry contract...");
+
   const LandRegistry = await ethers.getContractFactory("LandRegistry");
   const landRegistry = await LandRegistry.deploy();  
+  console.log("Contract details:", landRegistry);
 
-  console.log("LandRegistry deployed to:", landRegistry.address); 
+
+  
+  const address = landRegistry.address;
+  if (address) {
+    console.log("LandRegistry deployed to:", address);
+  } else {
+    console.error("Failed to deploy LandRegistry contract.");
+  }
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
+    console.error("An error occurred during deployment:", error);
     process.exit(1);
   });
---
+++ myPropertyBlockchain/script/deploySimple.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  console.log("Deploying SimpleStorage contract...");
+
+  const SimpleStorage =  await ethers.getContractFactory("SimpleStorage");
+  const simpleStorage =  SimpleStorage.deploy();
+  console.log("Contract details:", simpleStorage);
+//   await simpleStorage.deployed();
+
+  console.log("SimpleStorage deployed to:", simpleStorage.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("An error occurred during deployment:", error);
+    process.exit(1);
+  });
